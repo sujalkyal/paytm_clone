49f2d4516a606c5be36dbf87e9b68713afeb7871
sujalkyal <sujalkyal2704@gmail.com>
2024-10-04 13:23:53 +0530

    routing structure created

--- backend/db.js
+++ backend/db.js
@@ -10,11 +10,34 @@ mongoose.connect(MONGO_DB_URL)
     .catch((err)=>{console.log("error while connecting to the database")})
 
 const UserSchema = new Schema({
-    firstName : String , 
-    lastName : String , 
-    Password : Number
+    firstName : {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName : {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    username : {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    Password : {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 20
+    }
 });
 
 const User = mongoose.model('User',UserSchema);
 
-export { User };
+module.exports = { User };
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const app = express();
+const mainRouter = require("./routes/index");
+
+app.use('/api/v1',mainRouter);
+
+app.listen(port,()=>{
+    console.log('server is listening on port ${port}');
+});
